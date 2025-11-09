fce04d8faa08a0620923b7c883f6da5e3361fc6a
Tzipik <tzipik652@gmail.com>
2025-11-09 11:29:00 +0200

    update

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,13 @@
 // routes/authRoutes.js
 import express from 'express';
-import { register, login, googleLogin } from '../controllers/authController.js';
+import { register, login, googleLogin } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/google', googleLogin);
+
+router.get('/favorites', getFavorites);
 
 export default router;
