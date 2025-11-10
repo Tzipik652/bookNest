82236aea8fe1932d88559fb3b38cba892773e34f
Tzipik <tzipik652@gmail.com>
2025-11-10 11:34:47 +0200

    fix: update API routes for favorites to remove JWT verification on getBookLikes

--- client/src/services/bookService.ts
+++ client/src/services/bookService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Book, Favorite } from "../types";
+import { Book } from "../types";
 import { useUserStore } from "../store/useUserStore";
 
 const API_BASE_URL =
