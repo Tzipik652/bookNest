d524588e0f4aa82ea0b1d33baf798940931882c2
Tzipik <tzipik652@gmail.com>
2025-11-26 10:35:17 +0200

    fix(redisClient): validate REDIS_PORT and ensure correct usage in connection URL

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -1,9 +1,13 @@
 import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
+const portNumber = Number(process.env.REDIS_PORT);
+if (isNaN(portNumber) || portNumber <= 0 || portNumber > 65535) {
+  throw new Error(`REDIS_PORT is invalid: ${process.env.REDIS_PORT}`);
+}
 
 const redisClient = createClient({
-  url: `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${portNumber}`,
 });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
